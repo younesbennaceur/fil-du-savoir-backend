19b1c5837fa860229b7e13e2701ee7d7e763d114
younes BENNACEUR <younesbennaceur06@gmail.com>
2026-03-30 17:58:06 +0200

    fix(email): forcer smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,19 +5,14 @@ dotenv.config();
 
 // 1. Configurer le "Transporteur" (Celui qui livre le mail)
 // 1. Configurer le "Transporteur" (Celui qui livre le mail)
-// 1. Configurer le "Transporteur" (Celui qui livre le mail)
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,               // 👈 On utilise le port 587 au lieu de 465
-  secure: false,           // 👈 Obligatoirement 'false' avec le port 587
-  requireTLS: true,        // 👈 Force la connexion sécurisée
+  host: 'smtp.gmail.com', // Remplace service: 'gmail' par ceci
+  port: 465,              // Ajoute le port sécurisé
+  secure: true,           // Active la sécurité SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false // 👈 Évite les blocages de sécurité stricts de Render
-  }
 });
 
 export const sendInscriptionEmails = async (inscription) => {
@@ -75,5 +70,8 @@ export const sendInscriptionEmails = async (inscription) => {
     console.log("✅ Emails de confirmation envoyés avec succès !");
   } catch (error) {
     console.error("❌ Erreur lors de l'envoi des emails :", error);
+      console.error("❌ Code:", error.code);
+  console.error("❌ Message:", error.message);
+  console.error("❌ Détail:", JSON.stringify(error, null, 2));
   }
 };
